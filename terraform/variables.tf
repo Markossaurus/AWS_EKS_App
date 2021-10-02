6be166e01ffe4fe6319eb4a1d4a62185725be7e4
markossaurus <mt140299@gmail.com>
2021-10-02 22:19:20 +0300

    added variables and .gitignore

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable AWS_ACCESS_KEY {
+  type        = string
+  description = "AWS access key value from tfvars"
+}
+
+variable AWS_SECRET {
+  type        = string
+  description = "AWS secret key from tfvars"
+}
+
