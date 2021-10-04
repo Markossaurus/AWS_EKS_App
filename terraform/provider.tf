e7b9e2ed935fdac6a5ecc5f0dbcf0706e342f7c1
Markossaurus <mt140299@gmail.com>
2021-10-04 13:36:52 +0300

    added IAM policies and roles, configured new better vpc settings using module

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -14,3 +14,16 @@ provider "aws"{
     access_key = var.AWS_ACCESS_KEY
     secret_key = var.AWS_SECRET
 }
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+
+data "aws_regions" "current" {
+
+  filter {
+    name   = "region-name"
+    values = ["eu-central-1", "eu-west-1", "eu-west-2"]
+  }
+}
