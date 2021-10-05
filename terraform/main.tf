bee32adf061c70cb6ae297408f5e82cfc649c9b8
Markossaurus <mt140299@gmail.com>
2021-10-05 14:19:59 +0300

    finished configuring infastracture for now

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,45 @@
+resource "aws_eks_cluster" "haproxy" {
+  name     = "haproxy"
+  role_arn = aws_iam_role.eks-cluster.arn
 
+  vpc_config {
+    subnet_ids = module.vpc.public_subnets
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSClusterPolicy,
+  ]
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.haproxy.endpoint
+}
+
+output "kubeconfig-certificate-authority-data" {
+  value = aws_eks_cluster.haproxy.certificate_authority[0].data
+}
+
+
+resource "aws_eks_node_group" "haprox" {
+  cluster_name    = aws_eks_cluster.haproxy.name
+  node_group_name = "haprox"
+  node_role_arn   = aws_iam_role.eks_nodes.arn
+  subnet_ids      = module.vpc.public_subnets
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 1
+    min_size     = 1
+  }
+
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -20,10 +20,10 @@ data "aws_availability_zones" "available" {
 }
 
 
-data "aws_regions" "current" {
+/* data "aws_regions" "current" {
 
   filter {
     name   = "region-name"
     values = ["eu-central-1", "eu-west-1", "eu-west-2"]
   }
-}
+} */
