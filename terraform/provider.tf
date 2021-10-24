2989b0415009191adcd0aa21eb1ab531b5c4bb83
markossaurus <mt140299@gmail.com>
2021-10-24 15:04:32 +0300

    debugging terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -25,8 +25,22 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_eks_cluster_auth" "haproxy" {
+  name = var.cluster_name
+    depends_on = [
+    aws_eks_cluster.haproxy,
+    aws_eks_node_group.haprox,
+  ]
+}
+
+
 provider "kubernetes" {
   host                   = aws_eks_cluster.haproxy.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.haproxy.certificate_authority[0].data)
-  token                  = aws_eks_cluster_auth.haproxy.token
+  token                  = data.aws_eks_cluster_auth.haproxy.token
+
+    depends_on = [
+    aws_eks_cluster.haproxy,
+    aws_eks_node_group.haprox,
+  ]
 }
