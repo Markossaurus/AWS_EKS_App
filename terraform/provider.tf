cde4f49c8a0aa92618e195025a0e064abc8aa140
markossaurus <mt140299@gmail.com>
2021-10-24 15:11:50 +0300

    created deployement file for k8s and removed k8s provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,43 +4,14 @@ terraform {
         source = "hashicorp/aws"
         version = "~> 3.0"
     }
-    kubernetes = {
-        source = "hashicorp/kubernetes"
-        version = "2.5.0"
-        }
-    }
+  }
 }
-
-
-
 provider "aws"{
     profile = "default"
     region = "eu-central-1"
     access_key = var.AWS_ACCESS_KEY
     secret_key = var.AWS_SECRET
 }
-
-
 data "aws_availability_zones" "available" {
   state = "available"
-}
-
-data "aws_eks_cluster_auth" "haproxy" {
-  name = var.cluster_name
-    depends_on = [
-    aws_eks_cluster.haproxy,
-    aws_eks_node_group.haprox,
-  ]
-}
-
-
-provider "kubernetes" {
-  host                   = aws_eks_cluster.haproxy.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.haproxy.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.haproxy.token
-
-    depends_on = [
-    aws_eks_cluster.haproxy,
-    aws_eks_node_group.haprox,
-  ]
 }
